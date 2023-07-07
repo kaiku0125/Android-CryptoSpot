d48591a0a9e1f8f10278a304e4367034fd351336
Kaiku <pianoprince0125@gmail.com>
2023-07-08 00:56:37 +0800

    [feat] Add buildSrc

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,28 @@
+object Dependencies {
+    /**
+     * kotlin
+     */
+    const val KOTLIN_CORE = "androidx.core:core-ktx:${Versions.KOTLIN}"
+
+
+    /**
+     * Compose ui
+     */
+    const val COMPOSE_UI = "androidx.compose.ui:ui:${Versions.COMPOSE}"
+    const val COMPOSE_UI_PREVIEW = "androidx.compose.ui:ui-tooling-preview:${Versions.COMPOSE}"
+    const val COMPOSE_LIVEDATA = "androidx.compose.runtime:runtime-livedata:${Versions.COMPOSE}"
+    /**
+     * Compose ui test
+     */
+    const val COMPOSE_UI_TEST = "androidx.compose.ui:ui-test-junit4:${Versions.COMPOSE}"
+    const val COMPOSE_UI_TEST_TOOLING = "androidx.compose.ui:ui-tooling:${Versions.COMPOSE}"
+    const val COMPOSE_UI_TEST_MANIFEST = "androidx.compose.ui:ui-test-manifest:${Versions.COMPOSE}"
+
+    /**
+     * Material 3
+     */
+    const val COMPOSE_MATERIAL =  "androidx.compose.material:material:${Versions.COMPOSE_MATERIAL}"
+
+
+
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,14 @@
+object Versions {
+    /**
+     * kotlin
+     */
+    const val KOTLIN = "1.7.0"
+
+    /**
+     * Compose ui
+     */
+    const val COMPOSE = "1.2.0"
+    const val COMPOSE_MATERIAL = "1.2.0"
+
+
+}
