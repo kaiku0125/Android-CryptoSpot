b6ece63dda1401e2712109a29567d26f4ae2f8e0
Kaiku <pianoprince0125@gmail.com>
2023-08-09 16:47:43 +0800

    [feat] Add compose dialog component

--- app/src/main/java/com/kaiku/cryptospot/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/kaiku/cryptospot/presentation/home/HomeScreen.kt
@@ -7,12 +7,18 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.kaiku.cryptospot.customView.dialog.CustomDialog
+import com.kaiku.cryptospot.customView.topappbar.ScaffoldTopAppBarWithBackNavComponent
 import com.kaiku.cryptospot.navigation.FindCryptoDestination
 import com.kaiku.cryptospot.navigation.PocketHomeworkDestination
 import com.kaiku.cryptospot.navigation.ScreenNavigator
 import com.kaiku.cryptospot.navigation.TestDestination
+import com.kaiku.cryptospot.presentation.MainActivity
 import com.kaiku.cryptospot.presentation.theme.text_15sp
 import com.kaiku.cryptospot.presentation.theme.text_16ssp_bold
 import timber.log.Timber
@@ -21,8 +27,24 @@ import timber.log.Timber
 @Composable
 fun HomeScreen() {
     Timber.e("Now in home screen")
+    val context = LocalContext.current
     Scaffold(
-        topBar = { MyTopBar() }
+        topBar = {
+            ScaffoldTopAppBarWithBackNavComponent(
+                titleText = "現貨損益",
+                actions = {
+                    IconButton(
+                        onClick = {
+                            ScreenNavigator.to(FindCryptoDestination.route)
+                        }
+                    ) {
+                        Icon(Icons.Default.Add, contentDescription = "Add")
+                    }
+                }
+            ) {
+                (context as MainActivity).finish()
+            }
+        }
     ) { innerPadding ->
 
         Column(
@@ -54,6 +76,30 @@ fun HomeScreen() {
             ) {
                 Text(
                     text = "Tab與Spinner實作",
+                    style = MaterialTheme.typography.text_16ssp_bold
+                )
+            }
+
+            val showDialog = remember { mutableStateOf(false) }
+            if (showDialog.value) {
+                CustomDialog(
+                    value = "",
+                    setShowDialog = {
+                        showDialog.value = it
+                    },
+                    setValue = {
+                        Timber.e("set value : $it")
+                    }
+                )
+            }
+            Button(
+                modifier = Modifier.padding(start = 20.dp, top = 10.dp),
+                onClick = {
+                    showDialog.value = true
+                }
+            ) {
+                Text(
+                    text = "Dialog測試",
                     style = MaterialTheme.typography.text_16ssp_bold
                 )
             }
