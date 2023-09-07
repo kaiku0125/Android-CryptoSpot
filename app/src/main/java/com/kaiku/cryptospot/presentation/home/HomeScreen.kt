5a0fee92fdf0c4f34141f09f775aea74325ede72
Kaiku <pianoprince0125@gmail.com>
2023-09-07 17:15:47 +0800

    [upgrade] 升級 Kotlin 到 v1.8.0, Compose 到 v1.3.1, Material3 到 v1.1.1

--- app/src/main/java/com/kaiku/cryptospot/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/kaiku/cryptospot/presentation/home/HomeScreen.kt
@@ -1,16 +1,31 @@
 package com.kaiku.cryptospot.presentation.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.kaiku.cryptospot.customView.dialog.CustomDialog
 import com.kaiku.cryptospot.customView.dialog.EdmDialog
@@ -94,7 +109,7 @@ fun HomeScreen() {
 //                )
                 EdmDialog(
                     onNotRemind = {},
-                    onDismiss = {showDialog.value = false}
+                    onDismiss = { showDialog.value = false }
                 )
             }
             Button(
@@ -108,6 +123,51 @@ fun HomeScreen() {
                     style = MaterialTheme.typography.text_16ssp_bold
                 )
             }
+
+            val sheetState = rememberModalBottomSheetState()
+
+            var isSheetOpen by rememberSaveable {
+                mutableStateOf(false)
+            }
+
+            Button(
+                onClick = {
+                    isSheetOpen = true
+                }
+            ) {
+                Text(text = "Bottom sheet")
+            }
+
+
+            if (isSheetOpen) {
+                ModalBottomSheet(
+                    sheetState = sheetState,
+                    onDismissRequest = {
+                        isSheetOpen = false
+                    }
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .requiredHeight(IntrinsicSize.Max)
+                            .background(Color.Green),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column {
+                            Text(text = "test", modifier = Modifier.background(Color.Magenta))
+                            Text(text = "test", modifier = Modifier.background(Color.Magenta))
+                            Text(text = "test", modifier = Modifier.background(Color.Magenta))
+                            Text(text = "test", modifier = Modifier.background(Color.Magenta))
+                            Text(text = "test", modifier = Modifier.background(Color.Magenta))
+                            Text(text = "test", modifier = Modifier.background(Color.Magenta))
+                            Text(text = "test", modifier = Modifier.background(Color.Magenta))
+                        }
+
+                    }
+                }
+            }
+
+
         }
 
     }
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,14 +2,14 @@ object Versions {
     /**
      * kotlin
      */
-    const val KOTLIN = "1.7.0"
+    const val KOTLIN = "1.8.0"
 
     /**
      * Compose ui
      */
-    const val COMPOSE = "1.2.0"
+    const val COMPOSE = "1.3.1"
     const val COMPOSE_MATERIAL = "1.2.0"
-    const val COMPOSE_MATERIAL3= "1.0.1"
+    const val COMPOSE_MATERIAL3= "1.1.1"
     const val NAV_ANIMATION = "0.28.0"
 
 
